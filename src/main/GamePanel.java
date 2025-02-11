d23046508845e9fb4760333e6b51eee612b2dfb1
Starfall10 <103335303+Starfall10@users.noreply.github.com>
2025-02-11 01:14:13 +0000

    Added Player class and changed movement code

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,35 +2,17 @@ package main;
 
 import inputs.KeyboardInput;
 import inputs.MouseInput;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-
-
-import static utilz.Constants.PlayerConstants.*;
-import static utilz.Constants.Directions.*;
-
 
 public class GamePanel extends JPanel {
 
     private MouseInput mouseInput;
-    private float xDelta = 100, yDelta = 100;
-    private BufferedImage img;
-    private BufferedImage[][] animations;
-    private int aniTick, aniIndex, aniSpeed = 15;
-    private int playerAction = IDLE;
-    private int playerDir = -1;
-    private boolean moving = false;
+    private Game game;
 
-    public GamePanel() {
+    public GamePanel(Game game) {
         mouseInput = new MouseInput(this);
-        
-        importImg();
-        loadAnimation();
+        this.game = game;
 
         setPanelSize();
         addKeyListener(new KeyboardInput(this));
@@ -38,36 +20,6 @@ public class GamePanel extends JPanel {
         addMouseMotionListener(mouseInput);
     }
 
-    private void loadAnimation() {
-        animations = new BufferedImage[9][6];
-
-        for (int j =0 ; j < animations.length; j++) {
-            for (int i = 0; i < animations[j].length;i++) {
-                animations[j][i] = img.getSubimage(i*64,j*40,64,40);
-            }
-        }
-
-
-    }
-
-    private void importImg() {
-        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
-
-        try {
-            assert is != null;
-            img = ImageIO.read(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                assert is != null;
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
 
     private void setPanelSize() {
         Dimension size = new Dimension(1280,800);
@@ -76,69 +28,20 @@ public class GamePanel extends JPanel {
         setMaximumSize(size);
     }
 
-    public void setDirection(int direction) {
-        this.playerDir = direction;
-        moving = true;
-    }
-
-    public void setMoving(boolean moving) {
-        this.moving = moving;
-    }
-
-
-
-
-    private void updateAnimationTick() {
-        aniTick++;
-        if(aniTick >= aniSpeed) {
-            aniTick = 0;
-            aniIndex++;
-            if(aniIndex>= GetSpriteAmount(playerAction)) {
-                aniIndex = 0;
-            }
-        }
-    }
-
-    public void setAnimation() {
-        if(moving)
-            playerAction = RUNNING;
-        else
-            playerAction = IDLE;
-    }
-
-    public void updatePos() {
-        if(moving) {
-            switch(playerDir) {
-                case LEFT:
-                    xDelta-=5;
-                    break;
-                case UP:
-                    yDelta-=5;
-                    break;
-                case RIGHT:
-                    xDelta+=5;
-                    break;
-                case DOWN:
-                    yDelta+=5;
-                    break;
-            }
-        }
-    }
 
     public void updateGame() {
-        updateAnimationTick();
-        setAnimation();
-        updatePos();
+
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-
-        g.drawImage(animations[playerAction][aniIndex],(int)xDelta,(int)yDelta, 256,160,null);
-
+        game.render(g);
     }
 
+    public Game getGame() {
+        return game;
+    }
 
 
 }
