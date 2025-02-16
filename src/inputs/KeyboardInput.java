ef1bcbb32f2c79775a07a952dfaff1770abce5a0
Starfall10 <103335303+Starfall10@users.noreply.github.com>
2025-02-16 16:32:55 +0000

    Added Gravity, Jumping, and JumpAnimation

--- src/inputs/KeyboardInput.java
+++ src/inputs/KeyboardInput.java
@@ -34,6 +34,9 @@ public class KeyboardInput implements KeyListener {
             case KeyEvent.VK_D:
                 gamePanel.getGame().getPlayer().setRight(true);
                 break;
+            case KeyEvent.VK_SPACE:
+                gamePanel.getGame().getPlayer().setJump(true);
+                break;
         }
     }
 
@@ -52,6 +55,10 @@ public class KeyboardInput implements KeyListener {
             case KeyEvent.VK_D:
                 gamePanel.getGame().getPlayer().setRight(false);
                 break;
+            case KeyEvent.VK_SPACE:
+                gamePanel.getGame().getPlayer().setJump(false);
+                break;
+
         }
     }
 }
