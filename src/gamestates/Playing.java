64b78fb7775e2e032b05ef3ee933f5cc3eb4454a
Starfall10 <103335303+Starfall10@users.noreply.github.com>
2025-03-07 21:06:28 +0000

    Made the enemy move

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -61,7 +61,7 @@ public class Playing extends State implements Statemethods{
         if(!paused) {
             levelManager.update();
             player.update();
-            enemyManager.update();
+            enemyManager.update(levelManager.getCurrentLevel().getLvlData());
         }
         else {
             pauseOverlay.update();
